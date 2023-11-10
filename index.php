6f2da28336c99c72b7d1cd5867618241552dbf2f
rodrigo <rodrigoalejandro.fernandez@estudiantes.uva.es>
2023-11-10 11:23:19 +0000

    a

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="wrapper">
+				<main>	
+          <div>
+	          <div class = "guest-box">
+              <div>
+                <form class = "login-form">
+                  <fieldset class = "login-form__fieldset">
+                    <!---->
+                    <!---->
+                    <!---->
+                    <h2 class = "login-form__headline">Iniciar Sesión</h2>
+
+                    <label for="login">Nombre de Usuario</label>
+                    <div class = "input-wrapper">
+                      <input id="login" type="text">
+                    </div>
+                    <label for="contrasenia">Contraseña</label>
+
+                    <div class = "input-wrapper">
+                      <input id="contrasenia" type="password">
+                    </div>
+
+                  <h1 class = "boton">Entrar</h1>
+                  </fieldset>
+                </form>
+              </div>
+            </div>
+          </div>
+				</main>
+        <a>
+          <img class="logo" src="assets/logo.png" alt="logo" width = 50>	
+        </a>
+		</div> 
+  </body>
+</html>
