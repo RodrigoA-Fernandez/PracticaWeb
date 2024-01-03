c8d05268ce6b985408af42264927713699812601
rodrigo <rodrigoalejandro.fernandez@estudiantes.uva.es>
2024-01-03 15:47:03 +0000

    a

--- gestionarEstudiantes.php
+++ gestionarEstudiantes.php
@@ -2,6 +2,33 @@
 <?php comprobarProfesor($conexionBD)?>
 <?php cabeceraPlantilla()?>
 
-<?php piePlantilla()?>
+<!-- <div class="mensajes-contenedorMensajes container-fluid"> -->
+<!--   <div class="alumnos-alumno row m-2"> -->
+<!--     <div class="col-sm" style='font-family: Segoe UI Bold;'> -->
+<!--       Nombre: -->
+<!--     </div> -->
+<!--     <div class="col-sm" style='font-family: "Segoe UI";'> -->
+<!--       Nombre -->
+<!--     </div> -->
+<!--     <div class="col-sm" style='font-family: Segoe UI Bold;'> -->
+<!--       Login: -->
+<!--     </div> -->
+<!--     <div class="col-sm" style='font-family: "Segoe UI";'> -->
+<!--       login@uva.es -->
+<!--     </div> -->
+<!--   </div> -->
+<!--    -->
+<!-- </div> -->
 
+<div class="container-md py-5">
+  <div class="col-lg-7 mx-auto bg-white rounded shadow">
+    <div class="table-responsive">
+      <table class="table table-fixed">
+
+      </table>
+    </div>
+  </div>
+</div>
+
+<?php piePlantilla()?>
 <?php include_once "inc/codigo_finalizacion.php"; ?>
